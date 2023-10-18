2d87a72e889d14e7dc09d0e66e3b5c7ce6a9e3b6
Faycal Kilali <53886202+faycalki@users.noreply.github.com>
2023-10-18 16:55:54 -0300

    Improvements for extensibility

--- src/Node.java
+++ src/Node.java
@@ -1,7 +1,15 @@
+/**
+ * This Node class is made for double-linked lists. However, it can also be used for single-linked lists.
+ * @implNote We've opted not to extend the Node class, as a lot of the code in LinkedList uses type Node<T>.
+  If we extend to say, DLLNode, we'll have to rewrite almost the entire code-base of LinkedList rather than merely extend it.
+ * @param <T>, any generic object is acceptable.
+ */
 
 public class Node<T>{
     private T data; // Node's reference to its inner data
     private Node<T> reference; // Reference to the next node
+
+        private Node<T> prevReference; // pointer to the preceding node in the chain
 
 
     /**
@@ -12,17 +20,33 @@ public Node(T inData)
         {
         data = inData;
         reference = null;
+        prevReference = null;
         }
 
+        /**
+         * Constructor for objects of class Node, for singly-linked lists
+         * @implNote to preserve extensibility, this class will exist for any number of linked-lists.
+         * @param inData the object to direct the pointer to
+         * @param inReference the next node to direct the pointer to in the chain of nodes
+         */
+        public Node(T inData, Node<T> inReference)
+        {
+                data = inData;
+                reference = inReference;
+        }
+
+
 /**
- * Constructor for objects of class Node
+ * Constructor for objects of class Node, for Doubly-Linked Lists
  * @param inData the object to direct the pointer to
- * @param inNode the next node to direct the pointer to in the chain of nodes
+ * @param inReference the next node to direct the pointer to in the chain of nodes
+ * @param inPrevReference the previous node to direct the pointer to in the chain of nodes
  */
-public Node(T inData, Node inNode)
+public Node(T inData, Node<T> inReference, Node<T> inPrevReference)
         {
         data = inData;
-        reference = inNode;
+        reference = inReference;
+        prevReference = inPrevReference;
         }
 
 public Node<T> getNext(){
@@ -39,6 +63,27 @@ public T getData(){
 
 private void setData(T inData){
         data = inData;
+        }
+
+
+        /*
+                Belwo are additions to form Doubly-Linked Lists to the Node class
+         */
+        /**
+         * Accessor method for previous node in the chain
+         * @return the previous node reference
+         */
+        public Node<T> getPrevReference(){
+
+                return prevReference;
+        }
+
+        /**
+         * Mutator method for the previous node in the chain
+         * @param previous what to set the previous node reference to
+         */
+        public void setPrevReference(Node<T> previous){
+                prevReference = previous;
         }
 
         }
--- src/NormalTest.java
+++ src/NormalTest.java
@@ -1,11 +1,11 @@
 public class NormalTest {
     public static void main(String args[]){
 
-
-        DoublyLinkedList<DLLNode<String>> list = new DoublyLinkedList<>();
-        DLLNode<String> node = new DLLNode<String>("1");
-        DLLNode<String> nextNode = new DLLNode<String>("2");
-        DLLNode<String> nextNextNode = new DLLNode<String>("3");
+/*
+        DoublyLinkedList<Node<String>> list = new DoublyLinkedList<>();
+        Node<String> node = new Node<String>("1");
+        Node<String> nextNode = new Node<String>("2");
+        Node<String> nextNextNode = new Node<String>("3");
         list.addToFront(node);
         list.addToFront(nextNode);
         list.addToFront(nextNextNode);
@@ -15,7 +15,7 @@ public class NormalTest {
         nextNextNode.setPrevReference(nextNode);
         System.out.println(list.toString());
         System.out.println(list.checkDataAtLocation(1));
-
+*/
 
 
         //System.out.println(list.toString());
